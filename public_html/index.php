f0817c51f8bd443c5d29600e2b2381bc0407fa17
vishal-pandey <pandeyvishal25@yahoo.com>
2017-08-20 02:05:37 +0530

    hello

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shinchan Nohara</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 <form method="post" action="./secure/test.php">
-	<input type="text" name="user"><br>
-	<input type="submit" name="submit">
+	<input type="text" name="user" class="form-group">
+	<input type="submit" name="submit" class="btn">
 </form>
+
+</body>
+</html>
