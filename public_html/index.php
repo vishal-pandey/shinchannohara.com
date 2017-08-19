b6e5500e0f0e0727ba5152616027f3410073bae7
vishal-pandey <pandeyvishal25@yahoo.com>
2017-08-20 02:10:56 +0530

    hello

--- public_html/index.php
+++ public_html/index.php
@@ -12,10 +12,30 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<form method="post" action="./secure/test.php">
+<form method="post" action="./secure/test.php" class="theform">
 	<input type="text" name="user" class="form-group">
 	<input type="submit" name="submit" class="btn">
 </form>
+<div class="theoutput"></div>
 
 </body>
 </html>
+<script type="text/javascript">
+			$(document).ready(function(){
+				$(".theform").submit(function(e) {
+				    var url = "./secure/test.php";
+
+				    $.ajax({
+				           type: "POST",
+				           url: url,
+				           data: $(this).serialize(), 
+				           success: function(data)
+				           {
+				               $(".theoutput").html(data);
+				           }
+				         });
+
+				    e.preventDefault(); 
+				});
+			});
+</script>
